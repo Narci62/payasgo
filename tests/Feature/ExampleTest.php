540ed9771727c1660f387d8b858d52c746c6dd4c
kodonou narcisse <narcissekodonou@gmail.com>
2025-09-29 12:07:13 +0100

    test commit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,15 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // $response->assertStatus(200);
+
+        $a = 2;
+        $b = 4;
+
+        $c = $a + $b;
+
+        $this->assertEquals(6, $c);
     }
 }
